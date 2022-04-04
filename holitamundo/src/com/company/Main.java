3a560692bccc7bf331e2e9f51978c3d4c14484c0
joseiesvigan <jrdmchepu@gmail.com>
2022-04-04 20:05:16 +0100

    multi

--- holitamundo/src/com/company/Main.java
+++ holitamundo/src/com/company/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 5;
+        int b = 5;
+        int multi = a*b;
 
+
+        System.out.println("El resultado es " + multi);
         System.out.println("Hola mundo, como estamos");
+
+
     }
 }
