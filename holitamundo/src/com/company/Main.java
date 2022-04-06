335dc88e76a6ca428fede1eabc2dbb8c22174d36
joseiesvigan <jrdmchepu@gmail.com>
2022-04-06 20:02:25 +0100

    multi + suma

--- holitamundo/src/com/company/Main.java
+++ holitamundo/src/com/company/Main.java
@@ -7,11 +7,15 @@ public class Main {
         int a = 5;
         int b = 5;
         int multi = a*b;
-
+        int suma = a+b;
+        int sumamulti = multi +suma;
 
         System.out.println("El resultado es " + multi);
 
+        System.out.println("El resultado de la suma de los dos factores es " + sumamulti);
+
         System.out.println("Hola mundo, como estamos");
+
 
 
     }
